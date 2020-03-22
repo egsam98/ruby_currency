db79250bb56433dfd4904e71ff21aa39e3e44d8c
egsam98 <egsam98@gmail.com>
2020-03-22 15:18:37 +0300

    FixerApiService added

---
+++ app/services/fixer_api_service.rb
@@ -0,0 +1,14 @@
+class FixerApiService
+  BASE_URL = Settings[:fixer_api][:base_url]
+  ACCESS_KEY = Settings[:fixer_api][:access_key]
+
+  # @raise [JSON::ParserError, Faraday::ConnectionFailed]
+  # @return [Hash]
+  def get_currency!
+    raw_body = Faraday.new(url: BASE_URL, params: {access_key: ACCESS_KEY})
+                   .get('/api/latest').body
+    body = JSON.parse raw_body
+    body[:warning] = "Unfortunately, we cannot provide rates for custom currency due to the service's free plan"
+    body
+  end
+end
