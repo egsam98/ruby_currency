7fbdafbe967b83857447d75f3b295ad721dd01c6
egsam98 <egsam98@gmail.com>
2020-03-24 02:22:07 +0300

    api documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/api-docs' => "application#api_docs"
+  get '/' => redirect('/api-docs')
   namespace :api do
     resources :currencies, only: [:show]
   end
